34cfba4ad1bf44206e681ab2bba84284b9290f37
Documenter.jl <documenter@juliadocs.github.io>
2024-12-11 11:10:01 +0000

    build based on a97a78a

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = TimeVaryingPeriodograms","category":"page"},{"location":"#TimeVaryingPeriodograms","page":"Home","title":"TimeVaryingPeriodograms","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"HiBanana Documentation for TimeVaryingPeriodograms.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [TimeVaryingPeriodograms]","category":"page"}]
+}
